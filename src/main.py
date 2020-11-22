017b2af1d96d3767012603b461297fa9e213f707
Esteban Martin <esteban.martin@epita.fr>
2020-11-22 14:45:20 -0500

    [REST] reworked and added REST handling functions

--- src/main.py
+++ src/main.py
@@ -1,13 +1,8 @@
-import requests
 import json
+from rest import RestQueries
 
 if __name__ == "__main__":
-    base_uri = "https://dolphin.jump-technology.com:8443/api/v1/"
 
-    with open("credentials.json", "r") as fd:
-        creds = json.loads(fd.read())
-
-    response = requests.get(base_uri + "asset",
-                            auth=requests.auth.HTTPBasicAuth(creds['username'],
-                                                             creds['password']))
-    print(response.text)
+    tmp = RestQueries.get("asset")
+    assets = json.loads(tmp)
+    print(type(assets), assets[0], type(assets[0]))
---
+++ src/rest.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+class RestQueries:
+
+    base_uri = "https://dolphin.jump-technology.com:8443/api/v1/"
+
+    creds = {}
+    with open("credentials.json", "r") as fd:
+        creds = json.loads(fd.read())
+    if len(creds) == 0:
+        raise("Error:\tcredentials.json could not be parsed")
+
+    def get(path):
+        res = requests.get(RestQueries.base_uri + path,
+                           auth=requests.auth.HTTPBasicAuth(
+                               RestQueries.creds['username'],
+                               RestQueries.creds['password']))
+        return res.text
+
+    def post(self, path, content):
+        res = requests.get(base_uri + path,
+                           data=content,
+                           auth=requests.auth.HTTPBasicAuth(creds['username'],
+                                                            creds['password']))
+        return res.text
+
+    def post(self, path, content):
+        res = requests.get(base_uri + path,
+                           data=content,
+                           auth=requests.auth.HTTPBasicAuth(creds['username'],
+                                                            creds['password']))
+        return res.text
